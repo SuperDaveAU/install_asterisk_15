a59888d76b1bbcfc63ea3b47dfb8577d2359aca2
SuperDaveAU <SuperDaveAU@users.noreply.github.com>
2018-07-13 21:15:13 +0100

    Update install_asterisk_15.sh

--- install_asterisk_15.sh
+++ install_asterisk_15.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-15-current.tar.gz -o asterisk-15-current.tar.gz
 tar -zxvf asterisk-15-current.tar.gz
-cd asterisk-15.1.0
+cd asterisk-15.*
 cd contrib/scripts/
 yum install -y epel-release
 ./install_prereq install
